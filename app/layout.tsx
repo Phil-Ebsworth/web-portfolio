d8aaa38d938a90673fc49a1ad667be970d68ac10
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-02 22:40:28 +0200

    change frontpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,51 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+"use client"
+
+import '@/app/global.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
- 
+import * as React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { NavigationMenuDemo } from "@/components/navigation-menu"
+
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <NavigationMenuDemo />
+              {children}
+            </main>
+          </SidebarProvider>
+          {children}
+        </ThemeProvider>
         <SpeedInsights />
-        </body>
+      </body>
     </html>
   );
 }
