d0115e6f1a8c991ef5b07e3a7593815ed9f9a9cf
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-04 15:57:58 +0200

    added github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/s
 import { AppSidebar } from "@/components/app-sidebar"
 import { Separator } from "@/components/ui/separator"
 import { usePathname } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 
 
@@ -37,14 +38,30 @@ export default function RootLayout({
           <AppSidebar variant="inset" collapsible='icon' className="border border-none rounded-r-xl shadow-md bg-white/5 h-screen w-52" />
           <SidebarInset>
             <header className="flex h-14 shrink-0 items-center transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                <div className="flex gap-4 items-center">
+              <div className="flex gap-4 items-center flex-1">
                 <SidebarTrigger className="scale-150" />
                 <Separator
                   orientation="vertical"
                   className="mr-2 data-[orientation=vertical]:h-4 bg-gray-400"
                 />
                 <h1 className="text-lg font-semibold">{pathname.slice(1)}</h1>
-                </div>
+              </div>
+              <Button variant="ghost" asChild size="icon" className="hidden sm:flex p-0 scale-125">
+                <a
+                  href="https://github.com/Phil-Ebsworth"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label="GitHub"
+                  className="dark:text-foreground flex items-center justify-center w-12 h-12"
+                >
+                  <img
+                    src="/github-mark-white.svg"
+                    alt="GitHub"
+                    className="w-5 h-5"
+                    style={{ display: "block" }}
+                  />
+                </a>
+              </Button>
             </header>
             <main>{children}</main>
           </SidebarInset>
