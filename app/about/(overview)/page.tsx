50071c5ba37740a956cf7a5bcbf8f970eb1a00cc
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-05 15:56:54 +0200

    about template page

---
+++ app/about/(overview)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OverviewPage: React.FC = () => {
+    return (
+        <main>
+            <h1>About Overview</h1>
+            <p>Welcome to the overview section of the About page.</p>
+        </main>
+    );
+};
+
+export default OverviewPage;
