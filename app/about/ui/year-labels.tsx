c21c51a0834dc405966c0d68607e45426bd06a7e
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-23 00:45:17 +0200

    longer timeline

--- app/about/ui/year-labels.tsx
+++ app/about/ui/year-labels.tsx
@@ -2,12 +2,12 @@ export default function YearLabels() {
     return (
         <div className="w-10">
                 <div className="flex flex-col h-full justify-between items-center ml-2 mr-2">
-                    {Array.from({ length: 10 }).map((_, i) => {
+                    {Array.from({ length: 11 }).map((_, i) => {
                         const year = new Date().getFullYear() - i;
                         return (
                             <div key={year} className="flex flex-col items-center">
                                 <span className="text-mb ">{year}</span>
-                                {i < 9 && <div className="h-20 bg-gray-400 my-1" />}
+                                {i < 9 && <div className="h-[10%] bg-gray-400 my-1 w-full" />}
                             </div>
                         );
                     })}
