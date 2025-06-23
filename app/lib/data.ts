fcca1d759c347e89d5b9940666b99144e3481d0b
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-23 22:37:57 +0200

    set projects local

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,7 @@
 import postgres from 'postgres';
 import { Project, } from './definitions';
+import fs from 'fs/promises';
+import path from 'path';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -10,5 +12,17 @@ export async function fetchProjects() {
     } catch (error) {
         console.error('Database Error: ', error);
         throw new Error('Failed to fetch projects data.');
+    }
+}
+
+export async function fetchProjectsFromJson() {
+    try {
+        const filePath = path.join(process.cwd(), 'app', 'data', 'projects.json');
+        const fileContents = await fs.readFile(filePath, 'utf-8');
+        const projects: Project[] = JSON.parse(fileContents);
+        return projects;
+    } catch (error) {
+        console.error('File Read Error: ', error);
+        throw new Error('Failed to fetch projects from JSON.');
     }
 }
--- app/main/projects/ui/projects-grid.tsx
+++ app/main/projects/ui/projects-grid.tsx
@@ -1,21 +1,26 @@
 import { ProjectCard } from "./project-card";
-import { fetchProjects } from "@/app/lib/data";
+import { fetchProjectsFromJson } from "@/app/lib/data";
+import { ProjectsGridSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from "react";
 
 export async function ProjectsGrid() {
-    const projects = await fetchProjects();
+    const projects = await fetchProjectsFromJson();
     return (
+        <Suspense fallback={<ProjectsGridSkeleton />}>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6 xl:gap-8 2xl:grid-cols-2 2xl:gap-10 max-w-7xl mx-auto mt-6">
             {projects.map((project) => (
-            <ProjectCard
-            key={project.id}
-            img={project.image_url}
-            title={project.title}
-            description={project.description}
-            content={project.content}
-            link={project.link}
-            /> 
+                <ProjectCard
+                    key={project.id}
+                    img={project.image_url}
+                    title={project.title}
+                    description={project.description}
+                    content={project.content}
+                    link={project.link}
+                />
             ))}
         </div>
-    )
+        </Suspense>
+    );
 }
+
 export default ProjectsGrid;
