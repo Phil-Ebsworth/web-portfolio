e2fc382239557662852b913e0f405b43d5b33e5c
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-06-30 00:47:23 +0200

    Revert "header always in front"
    This reverts commit 7ebb44d8342149c27a8672b0b938babcb5b2c46d.

--- app/ui/layout/header.tsx
+++ app/ui/layout/header.tsx
@@ -11,23 +11,23 @@ export function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="flex sticky top-0 items-center px-2 md:px-2 gap-2 z-50">
+    <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
       <SidebarInset>
-      <header className="flex sticky top-0 py-1.5 items-center px-2 md:px-2 gap-2 bg-inherit z-50">
-        <div className="flex gap-4 items-center flex-1">
-        <SidebarTrigger className="scale-150" />
-        <Separator
-          orientation="vertical"
-          className="mr-2 data-[orientation=vertical]:h-4 bg-gray-400"
-        />
-        <Structure path={pathname} />
-        </div>
-        <div className="flex items-center gap-4">
-        <GithubButton />
-        <ModeToggle />
-        </div>
-      </header>
-      </SidebarInset>
+            <header className="flex top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
+              <div className="flex gap-4 items-center flex-1">
+                <SidebarTrigger className="scale-150" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4 bg-gray-400"
+                />
+                <Structure path={pathname} />
+              </div>
+              <div className="flex items-center gap-4">
+              <GithubButton />
+              <ModeToggle />
+              </div>
+            </header>
+          </SidebarInset>
     </header>
   );
 }
